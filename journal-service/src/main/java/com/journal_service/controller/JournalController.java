473ebdb74679a0deedb38f9efd5e19b7bdbafd65
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2026-04-20 15:28:37 +0000

    Add health check endpoints for Journal and User services

--- journal-service/src/main/java/com/journal_service/controller/JournalController.java
+++ journal-service/src/main/java/com/journal_service/controller/JournalController.java
@@ -7,8 +7,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "Journal Service is running";
+    }
+}
 
 @RestController
 @RequestMapping("/api/journals")
--- user-service/src/main/java/com/user_service/controller/UserController.java
+++ user-service/src/main/java/com/user_service/controller/UserController.java
@@ -8,9 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.function.Supplier;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "User Service is running";
+    }
+}
 
 @RestController
 @RequestMapping("/api/users")
